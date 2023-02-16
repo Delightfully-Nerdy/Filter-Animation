c0e96365cf8869818972f5bac23f623c67e6eab5
Dalitso Kasonde <dkasonde11@gmail.com>
2023-02-16 09:33:48 +0200

    animate and final commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import Movie from './Movie';
+import Filter from './Filter';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
+
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopular();
+  }, [])
 
   const fetchPopular = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=cb91d9e1c514b4e3cd72936e076cac85&language=en-US&page=1');
     const movies = await data.json();
-    
+    setPopular(movies.results);
+    setFiltered(movies.results);
   }
 
   return (
     <div className="App">
-      <h1>
-        Hello
-      </h1>
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div
+        layout
+        className="popular-movies"
+      >
+        <AnimatePresence>
+          {filtered.map(
+            movie => {
+              return <Movie key={movie.id} movie={movie} />
+            }
+          )}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
